92ab0f7759eac5c9786ae761b5fb25e0dda54338
ca5 <catinice@outlook.com>
2025-12-22 11:04:31 +0530

    Initial Commit
    Change-Id: I9823e78d64735d6dc95fedf1ce54128bf1279a73

---
+++ traverse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  traverse
+//
+//  Created by ca5 on 22/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ traverse/traverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  traverseApp.swift
+//  traverse
+//
+//  Created by ca5 on 22/12/25.
+//
+
+import SwiftUI
+
+@main
+struct traverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
